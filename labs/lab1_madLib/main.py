bd54934c291405268332080e62a802ee0d918e3c
eddiegemayel <eddiegem16@gmail.com>
2014-05-06 00:11:45 -0400

    Added loop
    Loop to add a random number of people who live in the city

--- labs/lab1_madLib/main.py
+++ labs/lab1_madLib/main.py
@@ -9,6 +9,8 @@ names_array = []
 car_array = ["BMW", "Lambo", "Honda", "Ford"]
 villain_array = ["Darth Vader", "Green Goblin", "The Joker", "Lex Luthor"]
 
+import random
+
 
 
 #collecting inputs from the user
@@ -17,7 +19,7 @@ name2 = raw_input("Type a second Name! ")
 
 
 verb1 = raw_input("Type a past tense Verb! ")
-verb2 = raw_input("Type a second Verb! ")
+verb2 = raw_input("Type another past tense Verb! ")
 
 number1 = raw_input("Type a number between (and/or including) 0 and 3! ")
 number2 = raw_input("Type another number between (and/or including) 0 and 3! ")
@@ -54,21 +56,30 @@ def villain_select(num):
     elif num > 3 or num < 0:
         return "Dr. Evil"
 
+def random_people():
+    for i in range(0,1):
+        return random.randrange(10000)
 
 
 car = car_select(int(number1))
 villain = villain_select(int(number2))
 v_num = int(number3) * 2
 
+r_number = random_people()
+
+print r_number
+
 
 #print names_array
 #print class_verbs
 
 story = '''
-This is the story about {names_array[0]}. He was driving around in his {car}, when a villain named {villain} wanted to fight
-him. {villain} was {v_num} years old...which was 2X older than {names_array[0]}, who was only {number3}!
-This was a cause to fight...until his good friend {names_array[1]} came to save the day! {names_array[1]} came in and {verb1}
-{villain} right in the face!
+This is the story about {names_array[0]}. He/She was driving around in his/her {car}, when a villain named {villain} appeared
+out of nowhere! {villain} was {v_num} years old...which was 2X older than {names_array[0]}, who was a young {number3} years of age!
+{names_array[0]} called {villain} 'old'...this angered {villain} and was a cause to fight...until their good friend {names_array[1]}
+came to save the day! {names_array[1]} came in and {class_verbs[verb1]} {villain} right in the face! Ouch! {villain} {class_verbs[verb2]} to the floor and died.
+{names_array[1]} had saved the day, and their friend {names_array[0]}. Hoorah! The {r_number} people of the city were elated
+that the villain had been destroyed!
 
 '''
 
@@ -77,3 +88,4 @@ story = story.format(**locals())
 
 
 print story
+
