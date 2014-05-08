fe281f5dab2e50a2ef391b1214f9e8edbe08478e
eddiegemayel <eddiegem16@gmail.com>
2014-05-07 23:45:03 -0400

    Select input
    added a select input field for the user to choose the position they
    want to play in the futbol league

--- labs/lab2-simple-form/page.py
+++ labs/lab2-simple-form/page.py
@@ -10,8 +10,9 @@ class HTMLFormPage(object):
     <body>
         '''
         self.page_content = '''
-        <h1>Sign Up For The Soccer Team</h1>
+        <h1>Sign Up For The Futbol Team</h1>
         <form method="GET" action="">
+            <h2>Basic Info</h2>
             <label for="firstname">First Name:</label>
             <input type="text" name="firstname" placeholder="Enter Your First Name"/>
             <label for="lastname">Last Name:</label>
@@ -19,12 +20,21 @@ class HTMLFormPage(object):
             <label for="phone">Phone Number:</label>
             <input type="text" name="phone" placeholder="Enter Your Contact Phone Number"/>
             <br/>
+            <h2>What Days Are You Available?</h2>
             <label for="friday">Friday:</label>
             <input type="checkbox" name="days" value="friday" checked="checked"/>
             <label for="saturday">Saturday:</label>
             <input type="checkbox" name="days" value="saturday"/>
             <label for="sunday">Sunday:</label>
             <input type="checkbox" name="days" value="sunday"/>
+            <br/>
+            <h2>What Position Do You Play?</h2>
+            <select>
+                <option value="striker">Forward/Striker</option>
+                <option value="mid">Midfielder</option>
+                <option value="back">Defender</option>
+                <option value="keeper">Keeper</option>
+            </select>
             <button name="submit">Enter</button>
         </form>
         '''
