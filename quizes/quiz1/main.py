8061d67fe9eed36891566ee15b853191f69c6dfb
eddiegemayel <eddiegem16@gmail.com>
2014-05-07 22:24:15 -0400

    Added a few comments, also tried making
    beer an integer, still can't get it to work, maybe syntax is wrong

--- quizes/quiz1/main.py
+++ quizes/quiz1/main.py
@@ -1,7 +1,8 @@
+#collect user inputs
 height = raw_input("Enter a height: ")
 width = raw_input("Enter a width: ")
 
-
+#calculates area
 def calc_area(w, h):
     if w == h:
         area = w * h
@@ -15,15 +16,19 @@ def calc_area(w, h):
         return string
 
 
-newArea = calc_area(int(width), int(height))
+#call out area function
+new_area = calc_area(int(width), int(height))
 
-print newArea
+#print the results
+print new_area
 
 
-beer = raw_input("Enter your beer")
+beer = raw_input("Enter your beer number : ")
 
 def count_down(b):
     for i in range(b, 1):
         return b
 
-print count_down(beer)
+the_countdown = count_down(int(beer))
+
+print the_countdown
