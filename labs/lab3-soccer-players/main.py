dac09bb7185462222e3ebc54cb3a31af0ee4cd80
eddiegemayel <eddiegem16@gmail.com>
2014-05-12 22:54:14 -0400

    Setup basic classes
    classes setup for the soccer players, plus the home html page that will
    display

--- labs/lab3-soccer-players/main.py
+++ labs/lab3-soccer-players/main.py
@@ -5,7 +5,67 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        home_page = Page()
+        home_page.update()
+        home_page.title = "Home Page"
+
+        ronaldo = Player()
+
+
+
+        zlatan = Player()
+        pogba = Player()
+        yaya = Player()
+        rues = Player()
+
+
+
+        self.response.write(home_page.print_out())
+
+
+#############################################player class
+class Player(object):
+    def __init__(self):
+        self.name = ""
+        self.position = ""
+        self.team = ""
+        self.__games = 0
+        self.goals = 0
+        self.__goals_per_game = 0
+
+
+    def print_info(self):
+        #prints out any info
+        pass
+
+    @property
+    def games(self):
+        return self.__games
+
+    @games.setter
+    def games(self, value):
+        pass
+
+#############################################page class
+
+class Page(object):
+    def __init__(self):
+        self.__title = "Home Page"
+        self.__open = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{self.title}</title>
+    </head>
+    <body>"""
+        self.__content = ""
+        self.__close = """
+    </body>
+</html> """
+        self.__all = self.__open + self.__content + self.__close
+
+    
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
