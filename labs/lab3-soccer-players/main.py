8d362344d80023cd248885123644b697014084ab
eddiegemayel <eddiegem16@gmail.com>
2014-05-13 16:52:45 -0400

    Dynamic content added
    now that variables / getters & setters were up and working, added the
    content that will dynamically appear depending on which link the user
    will click on, including their stats and corresponding photos

--- labs/lab3-soccer-players/main.py
+++ labs/lab3-soccer-players/main.py
@@ -12,7 +12,7 @@ class MainHandler(webapp2.RequestHandler):
         self.ronaldo.name = "Christiano Ronaldo"
         self.ronaldo.position = "Forward"
         self.ronaldo.team = "Real Madrid"
-        self.ronaldo.games = 165.0
+        self.ronaldo.games_played = 165.0
         self.ronaldo.goals = 177.0
         self.ronaldo.calc_goals_per_game()
         print self.ronaldo.goals_per_game
@@ -21,7 +21,7 @@ class MainHandler(webapp2.RequestHandler):
         self.zlatan.name = "Zlatan Ibrahimovic"
         self.zlatan.position = "Forward"
         self.zlatan.team = "PSG"
-        self.zlatan.games = 66.0
+        self.zlatan.games_played = 66.0
         self.zlatan.goals = 55.0
         self.zlatan.calc_goals_per_game()
         print self.zlatan.goals_per_game
@@ -31,7 +31,7 @@ class MainHandler(webapp2.RequestHandler):
         self.pogba.name = "Paul Pogba"
         self.pogba.position = "Midfielder"
         self.pogba.team = "Juventus"
-        self.pogba.games = 56.0
+        self.pogba.games_played = 56.0
         self.pogba.goals = 11.0
         self.pogba.calc_goals_per_game()
         print self.pogba.goals_per_game
@@ -40,7 +40,7 @@ class MainHandler(webapp2.RequestHandler):
         self.yaya.name = "Yaya Toure"
         self.yaya.position = "Midfielder"
         self.yaya.team = "Manchester City"
-        self.yaya.games = 131.0
+        self.yaya.games_played = 131.0
         self.yaya.goals = 38.0
         self.yaya.calc_goals_per_game()
         print self.yaya.goals_per_game
@@ -49,19 +49,61 @@ class MainHandler(webapp2.RequestHandler):
         self.rues.name = "Marco Rues"
         self.rues.position = "Winger"
         self.rues.team = "Dortmund"
-        self.rues.games = 61.0
+        self.rues.games_played = 61.0
         self.rues.goals = 30.0
         self.rues.calc_goals_per_game()
         print self.rues.goals_per_game
 
         players = [self.ronaldo, self.zlatan, self.pogba, self.yaya, self.rues]
-        print players
 
 
         home_page.title = "Home Page"
         home_page.update()
 
-        
+
+
+
+        self.response.write(home_page.open + home_page.nav)
+
+        if self.request.GET:
+            player = int(self.request.GET['player'])
+            print player
+
+            name = players[player].name
+            position = players[player].position
+            team = players[player].team
+            games_played = players[player].games_played
+            goals = players[player].goals
+            goals_per = players[player].goals_per_game
+
+            home_page.title = name
+            home_page.update()
+
+            content='''<div id="contentArea">
+            <h3>{name}</h3>
+                <section id="labelArea">
+                    <p class="label"><strong>Position:</strong></p>
+                    <p class="label"><strong>Team:</strong></p>
+                    <p class="label"><strong>Games Played:</strong></p>
+                    <p class="label"><strong>Goals Scored:</strong></p>
+                    <p class="label"><strong>Average Goals Per Game Scored:</strong></p>
+                </section>
+
+                <section id="statArea">
+                    <p class="stats">{position}</p>
+                    <p class="stats">{team}</p>
+                    <p class="stats">{games_played}</p>
+                    <p class="stats">{goals}</p>
+                    <p class="stats">{goals_per}</p>
+                </section>
+                <img src="css/images/{player}.jpg"/>
+                </div>'''
+
+
+            content = content.format(**locals())
+
+            self.response.write(content)
+        self.response.write(home_page.close)
 
 
 #############################################---------------player class
@@ -70,7 +112,7 @@ class Player(object):
         self.name = ""
         self.position = ""
         self.team = ""
-        self.games = 0.0
+        self.games_played = 0.0
         self.goals = 0.0
         self.__goals_per_game = 0.0
 
@@ -85,7 +127,7 @@ class Player(object):
         self.__goals_per_game = value
 
     def calc_goals_per_game(self):
-        total = self.goals / self.games
+        total = self.goals / self.games_played
         self.__goals_per_game = total
 
 
@@ -93,37 +135,38 @@ class Player(object):
 
 class Page(object):
     def __init__(self):
-        self.__title = "Home Page"
-        self.__open = """
+        self.title = ""
+        self.open = """
 <!DOCTYPE html>
 <html>
     <head>
         <title>{self.title}</title>
+        <link rel="stylesheet" href="css/main.css" type="text/css" />
     </head>
     <body>"""
-        self.__content = """
+        self.nav = """
         <h1>Greatest Footballers In The World</h1>
         <h2>Click on a great footballer to learn more about their stats</h2>
         <ul>
-            <li><a href="?Player=ronaldo">Ronaldo</a></li>
-            <li><a href="?player=zlatan">Zlatan Ibrahimovic</a></li>
-            <li><a href="?player=pogba">Paul Pogba</a></li>
-            <li><a href="?player=yaya">Yaya Toure</a></li>
-            <li><a href="?player=rues">Marco Rues</a></li>
+            <li><a href="?player=0">Ronaldo</a></li>
+            <li><a href="?player=1">Zlatan</a></li>
+            <li><a href="?player=2">Paul Pogba</a></li>
+            <li><a href="?player=3">Yaya Toure</a></li>
+            <li><a href="?player=4">Marco Rues</a></li>
         </ul>
         """
-        self.__close = """
+        self.close = """
     </body>
 </html> """
 
-        self.__all = self.__open + self.__content + self.__close
+        self.all = self.open + self.nav + self.close
 
 
     def print_out(self):
-        return self.__all
+        return self.all
 
     def update(self):
-        self.__all = self.__all.format(**locals())
+        self.all = self.all.format(**locals())
 
 
 
