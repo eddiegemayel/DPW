552d1ff7d1e7ec10ce6d57accfe152666dcc018b
eddiegemayel <eddiegem16@gmail.com>
2014-05-12 23:53:44 -0400

    Player objects setup
    Will add property and setters later. Testing out the player objects and
    making sure they display

--- labs/lab3-soccer-players/main.py
+++ labs/lab3-soccer-players/main.py
@@ -6,21 +6,56 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         home_page = Page()
-        home_page.update()
         home_page.title = "Home Page"
 
         ronaldo = Player()
-
-
+        ronaldo.name = "Christiano Ronaldo"
+        ronaldo.position = "Forward"
+        ronaldo.team = "Real Madrid"
+        ro_ga = ronaldo.games = 165.0
+        ro_go = ronaldo.goals = 177.0
+        ronaldo.goals_per_game = ro_go / ro_ga
+        print ronaldo.goals_per_game
 
         zlatan = Player()
+        zlatan.name = "Zlatan Ibrahimovic"
+        zlatan.position = "Forward"
+        zlatan.team = "PSG"
+        z_ga = zlatan.games = 66.0
+        z_go = zlatan.goals = 55.0
+        zlatan.goals_per_game = z_go / z_ga
+        print zlatan.goals_per_game
+
+
         pogba = Player()
+        pogba.name = "Paul Pogba"
+        pogba.position = "Midfielder"
+        pogba.team = "Juventus"
+        p_ga = pogba.games = 56.0
+        p_go = pogba.goals = 11.0
+        pogba.goals_per_game = p_go / p_ga
+        print pogba.goals_per_game
+
         yaya = Player()
+        yaya.name = "Yaya Toure"
+        yaya.position = "Midfielder"
+        yaya.team = "Manchester City"
+        y_ga = yaya.games = 131.0
+        y_go = yaya.goals = 38.0
+        yaya.goals_per_game = y_go / y_ga
+        print yaya.goals_per_game
+
         rues = Player()
+        rues.name = "Marco Rues"
+        rues.position = "Winger"
+        rues.team = "Dortmund"
+        re_ga = rues.games = 61.0
+        re_go = rues.goals = 30.0
+        rues.goals_per_game = re_go / re_ga
+        print rues.goals_per_game
 
 
-
-        self.response.write(home_page.print_out())
+        #self.response.write(home_page.print_out())
 
 
 #############################################player class
@@ -31,20 +66,30 @@ class Player(object):
         self.team = ""
         self.__games = 0
         self.goals = 0
-        self.__goals_per_game = 0
+        #self.goals_per_game = 0
 
 
     def print_info(self):
-        #prints out any info
         pass
 
-    @property
-    def games(self):
-        return self.__games
-
-    @games.setter
-    def games(self, value):
-        pass
+    # @property
+    # def games(self):
+    #     return self.__games
+    #
+    # @games.setter
+    # def games(self, value):
+    #     pass
+    #
+    #
+    #
+    # @property
+    # def goals_per_game(self):
+    #     return self.__goals_per_game
+    #
+    # @goals_per_game.setter
+    # def goals_per_game(self, goals, games):
+    #     new_value = goals / games
+    #     return new_value
 
 #############################################page class
 
@@ -64,7 +109,7 @@ class Page(object):
 </html> """
         self.__all = self.__open + self.__content + self.__close
 
-    
+
 
 
 app = webapp2.WSGIApplication([
