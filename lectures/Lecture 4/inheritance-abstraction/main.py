9b89f8bd1b4c75e3ecbc69194b96389d1faf52d7
eddiegemayel <eddiegem16@gmail.com>
2014-05-14 18:49:27 -0400

    LECTURE ACTIVITY

--- lectures/Lecture 4/inheritance-abstraction/main.py
+++ lectures/Lecture 4/inheritance-abstraction/main.py
@@ -2,7 +2,41 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        p = Page()
+
+
+class Page(object):
+    def __init__(self):
+        self._open = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        """
+        self._content = ""
+        self._close = """
+    </body>
+</html>"""
+        self._css_url = ""
+        self._title = ""
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, t):
+        self._title = t
+
+    @property
+    def css_url(self):
+        return self._css_url
+
+    @css_url.setter
+    def css_url(self, c):
+        self._css_url = c
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
