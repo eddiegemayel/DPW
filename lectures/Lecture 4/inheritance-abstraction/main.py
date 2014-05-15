d07f43a0c458ce94151cedaa66255f1879e3c76b
eddiegemayel <eddiegem16@gmail.com>
2014-05-14 21:51:13 -0400

    lecture
    lecture activities yao

--- lectures/Lecture 4/inheritance-abstraction/main.py
+++ lectures/Lecture 4/inheritance-abstraction/main.py
@@ -2,7 +2,14 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = Page()
+        p = FormPage()
+        p.title = "Yao"
+        p.form_header = "Enter ya name ya twat"
+        p.css_url = "css/styles.css"
+        self.response.write(p.print_out())
+
+        p2 = FormPage()
+
 
 
 class Page(object):
@@ -11,16 +18,18 @@ class Page(object):
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>{self.title}</title><!--calling the function-->
+        <link rel ="stylesheet" type="text/css" href="{self.css_url}"/>
     </head>
     <body>
         """
-        self._content = ""
+        self._content = "This is my DEFAULT content"
         self._close = """
     </body>
 </html>"""
         self._css_url = ""
         self._title = ""
+        self.all = ""
 
     @property
     def title(self):
@@ -37,6 +46,36 @@ class Page(object):
     @css_url.setter
     def css_url(self, c):
         self._css_url = c
+
+    def print_out(self):
+        self.update()
+        return self.all
+
+    def update(self):
+        self.all = self._open + self._content + self._close
+        self.all = self.all.format(**locals())
+
+class FormPage(Page):
+    def __init__(self):
+        #call that constructor hoe
+        Page.__init__(self)
+        #super(FormPage, self).__init__()
+
+        self.__form_open = """
+        <form method="GET" action="">"""
+        self.__inputs = """
+        <input type="text" name = "f_name" />
+        <input type="text" name = "l_name" />
+        <input type="submit" name = "submit" />
+        """
+        self.__form_close = """</form>"""
+        self.form_header = ">>Form Header<<"
+        self._content = self.form_header + self.__form_open + self.__inputs + self.__form_close
+
+
+    def update(self):
+        self.all = self._open + self.form_header + self.__form_open + self.__inputs + self.__form_close + self._close
+        self.all = self.all.format(**locals())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
