e9fb4a764572a7b6cb0b88bedd11bc7bb3131e8e
eddiegemayel <eddiegem16@gmail.com>
2014-05-21 17:41:40 -0400

    API push
    can't get it to return the value i want

--- final/final-project/main.py
+++ final/final-project/main.py
@@ -1,24 +1,37 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#Eddie
+#final project
+#may 19 2014
 import webapp2
+from page import *
+#import json , for json file
+from xml.dom import minidom #library for working with xml in python
+import urllib2
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        view = FormPage()
+        view.form_header = "Trailers"
+        view.title = "Movie trailers"
+        self.response.write(view.print_out())
+
+
+        if self.request.GET:
+            code = self.request.GET["code"]
+            url = "http://api.traileraddict.com/?film=" + code
+            #go get the api info
+            req = urllib2.Request(url)
+            opener = urllib2.build_opener()
+            data = opener.open(req)
+
+            #parse it
+            xmldoc = minidom.parse(data)
+
+
+            #look at elements within the xml
+            self.response.write(xmldoc.getElementsByTagName("title"))
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
