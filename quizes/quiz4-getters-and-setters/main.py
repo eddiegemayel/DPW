20cbaccb641f5f3c4beabf158db21adcdc26550c
eddiegemayel <eddiegem16@gmail.com>
2014-05-21 22:43:47 -0400

    Quiz 4
    created page class that holds html and link that will count up

--- quizes/quiz4-getters-and-setters/main.py
+++ quizes/quiz4-getters-and-setters/main.py
@@ -1,24 +1,43 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#Eddie Gemayel
+#Quiz 4
+#Getters and
+#Setters
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
+
         self.response.write('Hello world!')
+
+
+class Page(object):
+    def __init__(self):
+
+        self.__count = 0
+        self.open = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Quiz 4: Count</title>
+    </head>
+    <body>
+        """
+        self.count_area = """
+        {self.count}
+        """
+        self.__button = """
+        <a href=?count=button>Count Up</a>
+        """
+        self.close = """
+    </body>
+</html>
+        """
+
+    def update(self):
+        
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
