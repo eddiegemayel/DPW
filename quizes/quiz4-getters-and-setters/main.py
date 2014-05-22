1c9e6a6fafb51ba2a4fcc012006323f45fef1b25
eddiegemayel <eddiegem16@gmail.com>
2014-05-21 23:25:15 -0400

    tried it without page class
    still cannot get any local variables to format at all?

--- quizes/quiz4-getters-and-setters/main.py
+++ quizes/quiz4-getters-and-setters/main.py
@@ -7,26 +7,9 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-        page = Page()
 
-        global counter
+        page_counter = Counter()
 
-        if self.request.GET:
-            counter += 1
-
-        else:
-            counter = 0
-
-
-        page.count = counter
-
-        #page.update()
-        self.response.write(page.print_out())
-
-class Page(object):
-    def __init__(self):
-
-        self.__count = 0
         self.open = """
 <!DOCTYPE html>
 <html>
@@ -41,20 +24,35 @@ class Page(object):
 </html>
         """
 
+        global counter
 
-    def print_out(self):
+        if self.request.GET:
+            counter += 1
 
-        return self.open + self.close
+        else:
+            counter = 0
+
+
+        page_counter.count = counter
+
+        #page.update()
+        self.response.write(self.open+self.close)
+        self.response.write(page_counter.content + page_counter.button)
+
+class Page(object):
+    def __init__(self):
+        pass
 
 class Counter(object):
     def __init__(self):
+        self.__count = 0
         self.content = """
         {self.count}
         """
         self.__button = """
 
         <br/>
-        <a href='?count=count'>Count Up</a>
+        <a href='?count=counter'>Count Up</a>
         """
 
     @property
@@ -74,6 +72,9 @@ class Counter(object):
     def button(self, b):
         self.__button = b
 
+    def print_out(self):
+        self.content.format(**locals())
+        return self.content + self.__button
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
